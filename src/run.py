32e41d75125f2722941e0763e3810a7dd5dcb810
Ojan <jeancarlosdearaujo@gmail.com>
2026-01-16 00:45:31 -0300

    binding all together

--- src/run.py
+++ src/run.py
@@ -1,14 +1,36 @@
 import pygame
 from pygame.locals import QUIT
+
 from sys import exit
-from constants import BLACK
+
+from world.map_loader import MapLoader
+from world.entity_manager import EntityManager
+from systems.render_system import RenderingSystem
 
 pygame.init()
 pygame.display.set_caption("World of Tiles")
 
 width, height = 800, 600
-
 screen = pygame.display.set_mode((width, height))
+
+FOREST_MAP = [
+    "# # # # # # # # # # # # # # # # # # # #",
+    "# . . . . . . . . . . . . . . . . . . #",
+    "# . . ~ ~ ~ ~ . . . . . . . . . . . . #",
+    "# . . ~ . . ~ . . . . . . . . . . . . #",
+    "# . . ~ ~ ~ ~ . . . . . . . . . . . . #",
+    "# . . . . . . . . . . . . . . . . . . #",
+    "# . . . . . . . . . . . . . . . . . . #",
+    "# # # # # # # # # # # # # # # # # # # #"
+]
+
+
+entity_manager = EntityManager()
+
+map_loader = MapLoader(FOREST_MAP)
+map_loader.load_map(entity_manager)
+
+rendering_system = RenderingSystem(screen, entity_manager)
 
 while True:
     for event in pygame.event.get():
@@ -16,5 +38,6 @@ while True:
             pygame.quit()
             exit()
     
-    screen.fill(BLACK)
+    screen.fill((0, 0, 0))
+    rendering_system.render()
     pygame.display.update()
--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,1 +1,12 @@
 BLACK = (0, 0, 0)
+GRASS = 1
+SAND = 2
+WATER = 3
+
+TILE_SIZE = 32
+
+TILE_COLORS = {
+    GRASS: (34, 139, 34),    # Forest green
+    SAND: (184, 134, 11),    # Dark goldenrod
+    WATER: (0, 100, 200),    # Ocean blue
+}
