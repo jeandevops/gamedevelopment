88aa435f8742d020f9c32dfc13b501a9ee274ba5
Ojan <jeancarlosdearaujo@gmail.com>
2026-01-16 00:44:53 -0300

    create a rendering system

---
+++ src/systems/render_system.py
@@ -0,0 +1,35 @@
+from world.entity_manager import EntityManager
+from utils.constants import TILE_COLORS, TILE_SIZE
+import pygame
+
+class RenderingSystem:
+    def __init__(self, screen, entity_manager: EntityManager):
+        self.screen = screen
+        self.entity_manager = entity_manager
+
+    def render(self):
+        """Renders all tiles onto the screen"""
+
+        tiles = self._retrieve_tiles()
+        for entity_id, tile_component in tiles:
+            position_component = self.entity_manager.get_entity_by_id(entity_id)["position"]
+            tile_color = self._get_tile_color(tile_component.tile_type)
+            pygame.draw.rect(
+                self.screen,
+                tile_color,
+                pygame.Rect(
+                    position_component.x,
+                    position_component.y,
+                    TILE_SIZE,
+                    TILE_SIZE
+                )
+            )
+
+    def _retrieve_tiles(self) -> list[tuple[str, dict]]:
+        """Retrieves all tile entities from the EntityManager"""
+        tiles = self.entity_manager.get_entities_with_component("tile")
+        return tiles
+
+    def _get_tile_color(self, tile_type: int):
+        """Returns the color associated with a tile type"""
+        return TILE_COLORS.get(tile_type, (255, 0, 255))  # Default to magenta for unknown types
